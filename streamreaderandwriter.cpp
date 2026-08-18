1a6e7ff5d0207466736eed08f8736b6cc8a9aa43
mimyutt <mutiakhofifaturr@gmail.com>
2026-08-18 10:33:11 +0700

    menambahkan file dalam mode membaca dan menunjuk ke subuah file

--- streamreaderandwriter.cpp
+++ streamreaderandwriter.cpp
@@ -28,4 +28,9 @@ int main()
     }
     //menutup file setelah penulisan selesai
     outfile.close();
+
+    //membuka file dalam mode membaca
+    ifstream infile;
+    //menunjuk ke sebuah file
+    infile.open("contohfile.txt");
 }            
