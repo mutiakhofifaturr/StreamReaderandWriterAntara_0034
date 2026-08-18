9145c8b990be52fbb73acfbe962e8d4be71422d2
mimyutt <mutiakhofifaturr@gmail.com>
2026-08-18 10:15:12 +0700

    menambahkan deklarasi variabel baris, membuat input untuk nangkep nama file, dan membuat file dalam mode menulis

--- iowithpath.cpp
+++ iowithpath.cpp
@@ -4,3 +4,19 @@
 using namespace std;
 
 int main()
+{
+    //deklarasi variabel baris dan NamaFile
+    string baris, NamaFile;
+
+    //membuat input untuk menangkap nama file
+    cout << "Masukkan nama file : ";
+    cin >> NamaFile;
+
+    //membuat file dalam mode menulis
+    ofstream outfile;
+    //menunjuk ke sebuah nama file dengan ekstentsi txt
+    outfile.open("C:\\Users\\LENOVO\\Documents\\contoh\\" + NamaFile +
+    ".txt", ios :: out);
+
+    cout << ">= Menulis file, \'q\' untuk keluar" << endl;
+}
